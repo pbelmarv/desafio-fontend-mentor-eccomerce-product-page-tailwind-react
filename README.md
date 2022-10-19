34912820ec9807910a6a2e87eca95c87a17ba433
pbelmarv <pbelmarv@hotmail.com>
2022-10-18 23:43:09 -0300

    dia 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Desafio Frontend Mentor
+- [Revisa el desafio aqui](https://www.frontendmentor.io/challenges/ecommerce-product-page-UPsZ9MJp6)
+- [preview project]()
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "@/components/header/mainHeader";
+import MainProducts from "@/components/products/mainProducts";
+const App = () => {
+    return (
+        <>
+            <Header />
+            <MainProducts />
+        </>
+    );
+};
+
+export default App;
